45241e8f399217c2176cf505164fb04c871affbf
catu <catu@sombrero.com.br>
2011-12-10 13:43:41 +0000

    Atualização da Fase 2 (Carlos Moffatt)

--- Asteroid/Asteroid/Fase2.cs
+++ Asteroid/Asteroid/Fase2.cs
@@ -12,34 +12,47 @@ using Microsoft.Xna.Framework.Media;
 namespace Asteroid
 {
     /// <summary>
-    /// carlos
+    /// Carlos Moffatt
     /// </summary>
-    class Fase2
-    {
+    class Fase2 {
         Boolean playing_musica;
         Song musica;
-        Texture2D fundo;
+        Texture2D texturaFundo;
+        Texture2D texturaNave;
+        Vector2 tamanhoStage;
+        Vector2 tamanhoFundo;
+        Vector2 posFundo;
+        Vector2 posicao_j1;
+        NaveFase2 jogador1;
 
-        public Fase2(ContentManager Content)
-        {
-            //carregar coisas da fase2
-
+        public Fase2(ContentManager Content) {
+            playing_musica = false;
+            musica = Content.Load<Song>("musica_fase2");
+            texturaFundo = Content.Load<Texture2D>("fundoFase2");
+            tamanhoFundo.X = texturaFundo.Bounds.Width;
+            tamanhoFundo.Y = texturaFundo.Bounds.Height;
+            texturaNave = Content.Load<Texture2D>("naveFase2");
+            tamanhoStage.X = 800;
+            tamanhoStage.Y = 480;
+            posicao_j1.X = (tamanhoStage.X - texturaNave.Bounds.Width) / 2;
+            posicao_j1.Y = (tamanhoStage.Y - texturaNave.Bounds.Height) / 2;
+            jogador1 = new NaveFase2(1, texturaNave, posicao_j1, Color.White, 0.0f, "teste", 2, 0, Content, tamanhoStage);
         }
 
-        public void Update(
-           GameTime time,
-           KeyboardState teclado,
-           KeyboardState tecladoanterior
-           )
+        public void Update(GameTime gameTime, KeyboardState teclado, KeyboardState tecladoAnterior)
         {
-
+            if (!playing_musica) {
+                MediaPlayer.Play(musica);
+                playing_musica = true;
+            }
+            posFundo.X = (tamanhoStage.X / 2) - (tamanhoFundo.X / 2);
+            posFundo.Y = (tamanhoStage.Y / 2) - (tamanhoFundo.Y / 2);
+            jogador1.Update(gameTime, teclado, tecladoAnterior);
         }
 
-        public void Draw(
-            GameTime gameTime,
-            SpriteBatch spriteBatch)
-        {
-
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch) {
+            spriteBatch.Draw(texturaFundo, posFundo, Color.White);
+            jogador1.Draw(gameTime, spriteBatch);
         }
     
     }//fim da classe
