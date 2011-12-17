bed29eb1d85f7cddb6d3d86d05d162d3c12f3527
christianandrande@gmail.com <christianandrande@gmail.com@bb524c01-ae89-1763-e2a0-059847d9df6c>
2011-12-17 15:42:19 +0000

    Fase 15

--- trunk/Asteroid/Asteroid/Estados/Fase15/Fase15.cs
+++ trunk/Asteroid/Asteroid/Estados/Fase15/Fase15.cs
@@ -1,11 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace Asteroid
 {
+    /// <summary>
+    /// Christian Andrade
+    /// </summary>
     class Fase15
     {
+        Boolean playing_musica;
+        Song musica;
+        Texture2D backgroundTexture;
+        Vector2 bgPosition;
+        Nave_jogador Nave;
+        Texture2D naveTexture;
+        Objeto objetosemtipo;
+        Texture2D desenhoParam;
+        Vector2 posicao;
+        Vector2 stageSize;
+        GameWindow gw;
+
+         public Fase15(ContentManager Content, GameWindow gw){
+            
+        }
+        public void Update(GameTime time, KeyboardState teclado, KeyboardState tecladoanterior)
+        {
+           
+        }
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch){
+            spriteBatch.Draw(backgroundTexture, bgPosition, Color.White);
+        }
+
     }
 }
