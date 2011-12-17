96d0a1cb9da8e656e49c40520153477f4b25aac3
correiopatlabor <correiopatlabor@hotmail.com>
2011-12-17 15:52:47 +0000

    adicionado nova tela de creditos

--- Asteroid/Asteroid/Estados/Fase06/Fase6.cs
+++ Asteroid/Asteroid/Estados/Fase06/Fase6.cs
@@ -1,7 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 
 namespace Asteroid
 {
@@ -10,5 +16,15 @@ namespace Asteroid
     /// </summary>
     class Fase6
     {
+        Boolean playing_musica;
+        Song musica;
+        Texture2D texturaFundo;
+        Texture2D texturaNave;
+        Vector2 tamanhoStage;
+        Vector2 tamanhoFundo;
+        Vector2 posFundo;
+        Vector2 posicao_j1;
+        NaveFase2 jogador1;
+
     }
 }
