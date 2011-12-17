a5826fa2fa8874f2f3be47451c9f25360c9f0614
catu <catu@sombrero.com.br>
2011-12-17 14:46:00 +0000

    Nave inimiga

--- Asteroid/Asteroid/Ship_enemy.cs
+++ Asteroid/Asteroid/Ship_enemy.cs
@@ -11,7 +11,108 @@ using Microsoft.Xna.Framework.Media;
 
 namespace Asteroid
 {
-    class Nave_inimigo
+    /// <summary>
+    /// Classe Nave do inimigo HERDANDO as características dos objetos que aparecem em cena
+    /// </summary>
+    class Nave_inimigo : Objeto
     {
-    }
-}
+        int inimigo;
+        int tiros;
+
+        TiroFase2 tiro;
+        //SoundEffect tiroSom;
+        bool atirando;
+
+        public Nave_inimigo(
+            int inimigo,
+            Texture2D textura,
+            Vector2 posicao,
+            float angulo,
+            GameWindow gw,
+            int tiros,)
+            : base(
+            textura,
+            posicao,
+            angulo,
+            gw)
+        {
+            this.inimigo = inimigo;
+            atirando = false;
+            this.tiros = tiros;
+        }
+
+
+        public void Update(GameTime _gameTime)
+        {
+            // A nave inimiga começa com 15 tiros
+            // A nave gira para a direção que está a nave do jogador e ATIRA
+            // SE ACABAR A MUNIÇÃO
+                // A nave gira para a direção que está a nave do jogador, e acelera (Tipo nave Kamikaze)
+
+            //angulo -= 2;
+            
+            // ACELERAÇÃO
+            //velocidade.X += (float)Math.Cos(Math.PI * angulo / 180) * 0.02f;
+            //velocidade.Y += (float)Math.Sin(Math.PI * angulo / 180) * 0.02f;
+
+            // ATIROU COICE do tiro
+            //velocidade.X -= (float)Math.Cos(Math.PI * angulo / 180) * 0.3f;
+            // velocidade.Y -= (float)Math.Sin(Math.PI * angulo / 180) * 0.3f;
+                    //tiro = new TiroFase2(tipoTiro, posicao, janela, angulo, Content);
+                    //atirando = true;
+
+            //#region Verificar os limites de velocidade (Falta parametrizar)
+            //if (velocidade.X > 4) { velocidade.X = 4; }
+            //if (velocidade.Y > 4) { velocidade.Y = 4; }
+            //if (velocidade.X < -4) { velocidade.X = -4; }
+            //if (velocidade.Y < -4) { velocidade.Y = -4; }
+            //#endregion
+
+            //posicao += velocidade;
+
+            #region Verifica nave nos limites da tela
+            if (posicao.X < 0)
+            {
+                posicao.X = gw.ClientBounds.Width;
+            }
+            else if (posicao.X > gw.ClientBounds.Width)
+            {
+                posicao.X = 0;
+            }
+
+            if (posicao.Y < 0)
+            {
+                posicao.Y = gw.ClientBounds.Height;
+            }
+            else if (posicao.Y > gw.ClientBounds.Height)
+            {
+                posicao.Y = 0;
+            }
+            #endregion
+
+            if (atirando)
+            {
+                tiro.Update(_gameTime);
+            }
+
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch sb)
+        {
+            sb.Draw(
+                textura,
+                posicao,
+                new Rectangle(0, 0, textura.Width, textura.Height),
+                cor,
+                MathHelper.ToRadians(angulo),
+                new Vector2(textura.Width / 2, textura.Height / 2),
+                1,
+                SpriteEffects.None,
+                0);
+
+            //sb.Draw(textura, Vector2.Zero, Color.White);
+        }
+
+
+    }//fim classe
+}//fim namespace
