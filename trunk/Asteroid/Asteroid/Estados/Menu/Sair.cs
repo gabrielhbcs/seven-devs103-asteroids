8101258008d94895ab04a9556b412fde1f98b197
gabriel_hbcs@hotmail.com <gabriel_hbcs@hotmail.com@bb524c01-ae89-1763-e2a0-059847d9df6c>
2011-12-25 05:41:47 +0000

    Adicionamento da tela de menu e concerto da fase 07

---
+++ trunk/Asteroid/Asteroid/Estados/Menu/Sair.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Asteroid
+{
+    class Sair
+    {
+        Texture2D textura;
+        Vector2 pos;
+        string endereco = "Estados/Menu/";
+        public Sair(ContentManager Content, int cont, Vector2 posicao)
+        {
+            if (cont == 4)
+            {
+                textura = Content.Load<Texture2D>(endereco + "sair2");
+            }
+            else
+            {
+                textura = Content.Load<Texture2D>(endereco + "sair");
+            }
+            pos = posicao;
+
+        }
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(textura, pos, Color.White);
+        }
+    }
+}
---
+++ trunk/Asteroid/Asteroid/Estados/Menu/menuCredito.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Asteroid
+{
+    class menuCredito
+    {
+        Texture2D textura;
+        Vector2 pos;
+        string endereco = "Estados/Menu/";
+        public menuCredito(ContentManager Content, int cont, Vector2 posicao)
+        {
+            if (cont == 3)
+            {
+                textura = Content.Load<Texture2D>(endereco + "creditos2");
+            }
+            else
+            {
+                textura = Content.Load<Texture2D>(endereco + "creditos");
+            }
+            pos = posicao;
+        }
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(textura, pos, Color.White);
+        }
+    }
+}
